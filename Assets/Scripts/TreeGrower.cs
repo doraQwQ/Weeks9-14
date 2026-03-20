58622c97a264500cad482e50459b36b99242cbd4
chengany <chengany@OA-S141-12>
2026-03-20 16:34:30 -0400

    Added Week 10 stuff

---
+++ Assets/Scripts/TreeGrower.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class TreeGrower : MonoBehaviour
+{
+    public AnimationCurve growCurve;
+    public Transform branchesTransform;
+    public float maxSpawnDistance;
+
+    public float duration;
+    public float apppleGrowDuration;
+    public GameObject applePrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Treegrow.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+
+public class TreeGrow : MonoBehaviour
+{
+    public AnimationCurve growCurve;
+    public Transform branchesTransform;
+    public float maxSpawnDistance;
+
+    public float duration;
+    public float appleGrowDuration;
+    public GameObject applePrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator TreeGrowUpdate()
+    {
+        float progress = 0f;
+
+        //The contents of the while loop run while the condition is true
+        while (progress < duration)
+        {
+            progress += Time.deltaTime;
+            transform.localScale = growCurve.Evaluate(progress / duration) * Vector3.one;
+
+            //Relinquishes control of Unity so that everything else can run
+            //For the rest of this frame
+            Debug.Log("How long has it been since the last frame?: " + Time.deltaTime);
+            yield return null;
+        }
+
+        //relinguish control of Unity so everything can run
+        Coroutine appleCoroutine = StartCoroutine(AppleGrowUpdate());  //grows 3 apples
+
+        //Wait until the appleCoroutine is done before moving on to the next line of code
+        yield return appleCoroutine;
+
+        //waits for the appleGrowDuration to elapse before starting the next apple grow
+        //yield return new WaitForSeconds( appleGrowDuration);  //delay --> appleGrowDuration
+        appleCoroutine = StartCoroutine(AppleGrowUpdate());
+
+        yield return appleCoroutine;
+
+        appleCoroutine = StartCoroutine(AppleGrowUpdate());
+        
+    }
+
+    private IEnumerator AppleGrowUpdate()
+    {
+        Vector3 spawnPosition = branchesTransform.position;
+        spawnPosition += (Vector3)UnityEngine.Random.insideUnitCircle * maxSpawnDistance;
+
+        GameObject spawnedApple = Instantiate(applePrefab, spawnPosition, Quaternion.identity);
+        spawnedApple.transform.localScale = Vector3.zero;
+        float progress = 0f;
+
+        while (progress < appleGrowDuration)
+        {
+            progress += Time.deltaTime;
+
+            spawnedApple.transform.localScale = growCurve.Evaluate(progress / appleGrowDuration) * Vector3.one;
+
+            //Relinquishes control of Unity so that everything else can run
+            //For the rest of this frame
+            Debug.Log("How long has it been since the last frame?: " + Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public void OnGrowPress()
+    {
+        StartCoroutine(TreeGrowUpdate());
+    }
+}
